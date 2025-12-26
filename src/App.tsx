30a5f4607224a35f3685ca7b1d139e85a345312f
frey2030 <frey2030@gmail.com>
2025-12-26 21:28:49 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { Route, Switch } from "wouter";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-// استخدام @ للإشارة إلى مجلد src مباشرة لتجنب أخطاء المسارات
-// تأكد أن الملف في GitHub اسمه Home.tsx (بـ H كبيرة)
-import Home from "@/pages/Home";
-import NotFound from "@/pages/NotFound";
-
-function Router() {
+function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
-  );
-}
-
-function App() {
-  return (
-    <TooltipProvider>
-      <Toaster />
-      <Router />
-    </TooltipProvider>
   );
 }
 
