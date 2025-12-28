4fe819e7360babb25bfe8e8f3cc233b6364a1e83
frey2030 <frey2030@gmail.com>
2025-12-28 18:19:24 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,20 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Timeline from "./components/Timeline";
+import StatsBar from "./components/StatsBar";
+import Services from "./components/Services";
+
+export default function App() {
+  return (
+    <div dir="rtl">
+      <Header />
+      <Hero />
+      <About />
+      <Timeline />
+      <StatsBar />
+      <Services />
+    </div>
+  );
+}
 
