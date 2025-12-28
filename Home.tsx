a9f235aec521b42d556acaf3524ed4305e2b1131
frey2030 <frey2030@gmail.com>
2025-12-28 18:25:05 +0200

    Create Home.tsx

---
+++ Home.tsx
@@ -0,0 +1,17 @@
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
+import ProductsSection from "@/components/ProductsSection";
+import ContactSection from "@/components/ContactSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <ProductsSection />
+      <ContactSection />
+    </>
+  );
+}
